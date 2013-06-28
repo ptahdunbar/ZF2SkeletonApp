ffca60a2e514f202eacd21580992cde59779f1d8
Ptah Dunbar <ptah@piratedunbar.com>
2013-06-28 14:12:03 +0200

    Updated translation strings to use keys. Explicitly added semi-colons. They arent cool, but lets stay consistent.

--- module/Application/language/en_US.php
+++ module/Application/language/en_US.php
@@ -1,113 +1,140 @@
 <?php
 
 return [
-    #: ../view/layout/layout.phtml:33
-    'Skeleton Application' => 'Skeleton Application',
+    #: ../view/layout/layout.phtml:6
+    #: ../view/layout/layout.phtml:40
+    '__label_app_name'
+        => 'Skeleton Application',
 
-    #: ../view/layout/layout.phtml:36
-    'Home' => 'Home',
+    #: ../view/layout/layout.phtml:43
+    '__label_home'
+        => 'Home',
 
-    #: ../view/layout/layout.phtml:50
-    'All rights reserved.' => 'All rights reserved.',
+    #: ../view/layout/layout.phtml:53
+    '__message_colophon'
+        => '&copy; 2005 - %s by Zend Technologies Ltd. All rights reserved.',
 
     #: ../view/application/index/index.phtml:2
-    'Welcome to %sZend Framework 2%s' => 'Welcome to %sZend Framework 2%s',
+    '__title_welcome_zf2'
+        => 'Welcome to %sZend Framework 2%s',
 
     #: ../view/application/index/index.phtml:3
-    'Congratulations! You have successfully installed the %sZF2 Skeleton Application%s. You are currently running Zend Framework version %s. This skeleton can serve as a simple starting point for you to begin building your application on ZF2.'
-    => 'Congratulations! You have successfully installed the %sZF2 Skeleton Application%s. You are currently running Zend Framework version %s. This skeleton can serve as a simple starting point for you to begin building your application on ZF2.',
-
-    #: ../view/application/index/index.phtml:3
-    'Congratulations! You have successfully installed the %sZF2 Skeleton Application%s. You are currently running Zend Framework version %s. This skeleton can serve as a simple starting point for you to begin building your application on ZF2.'
-    => 'Congratulations! You have successfully installed the %sZF2 Skeleton Application%s. You are currently running Zend Framework version %s. This skeleton can serve as a simple starting point for you to begin building your application on ZF2.',
+    '__message_welcome_zf2'
+        => 'Congratulations! You have successfully installed the %sZF2 Skeleton Application%s. You are currently running Zend Framework version %s. This skeleton can serve as a simple starting point for you to begin building your application on ZF2.',
 
     #: ../view/application/index/index.phtml:4
-    'Fork Zend Framework 2 on GitHub' => 'Fork Zend Framework 2 on GitHub',
+    '__label_fork_on_github'
+        => 'Fork Zend Framework 2 on GitHub',
 
     #: ../view/application/index/index.phtml:10
-    'Follow Development' => 'Follow Development',
+    '__title_follow_development'
+        => 'Follow Development',
 
     #: ../view/application/index/index.phtml:11
-    'Zend Framework 2 is under active development. If you are interested in following the development of ZF2, there is a special ZF2 portal on the official Zend Framework website which provides links to the ZF2 %swiki%s, %sdev blog%s, %sissue tracker%s, and much more. This is a great resource for staying up to date with the latest developments!'
-    => 'Zend Framework 2 is under active development. If you are interested in following the development of ZF2, there is a special ZF2 portal on the official Zend Framework website which provides links to the ZF2 %swiki%s, %sdev blog%s, %sissue tracker%s, and much more. This is a great resource for staying up to date with the latest developments!',
+    '__message_follow_development'
+        => 'Zend Framework 2 is under active development. If you are interested in following the development of ZF2, there is a special ZF2 portal on the official Zend Framework website which provides links to the ZF2 %swiki%s, %sdev blog%s, %sissue tracker%s, and much more. This is a great resource for staying up to date with the latest developments!',
 
     #: ../view/application/index/index.phtml:12
-    'ZF2 Development Portal' => 'ZF2 Development Portal',
+    '__label_zf2_portal'
+        => 'ZF2 Development Portal',
 
     #: ../view/application/index/index.phtml:16
-    'Discover Modules' => 'Discover Modules',
+    '__title_discover_modules'
+        => 'Discover Modules',
 
     #: ../view/application/index/index.phtml:17
-    'The community is working on developing a community site to serve as a repository and gallery for ZF2 modules. The project is available %son GitHub%s. The site is currently live and currently contains a list of some of the modules already available for ZF2.'
-    => 'The community is working on developing a community site to serve as a repository and gallery for ZF2 modules. The project is available %son GitHub%s. The site is currently live and currently contains a list of some of the modules already available for ZF2.',
+    '__message_discover_modules'
+        => 'The community is working on developing a community site to serve as a repository and gallery for ZF2 modules. The project is available %son GitHub%s. The site is currently live and currently contains a list of some of the modules already available for ZF2.',
 
     #: ../view/application/index/index.phtml:18
-    'Explore ZF2 Modules' => 'Explore ZF2 Modules',
+    '__label_explore_modules'
+        => 'Explore ZF2 Modules',
 
     #: ../view/application/index/index.phtml:22
-    'Help &amp; Support' => 'Help &amp; Support',
+    '__title_get_support'
+        => 'Help &amp; Support',
 
     #: ../view/application/index/index.phtml:23
-    'If you need any help or support while developing with ZF2, you may reach us via IRC: %s#zftalk on Freenode%s. We\'d love to hear any questions or feedback you may have regarding the beta releases. Alternatively, you may subscribe and post questions to the %smailing lists%s.'
-    => 'If you need any help or support while developing with ZF2, you may reach us via IRC: %s#zftalk on Freenode%s. We\'d love to hear any questions or feedback you may have regarding the beta releases. Alternatively, you may subscribe and post questions to the %smailing lists%s.',
+    '__message_get_support'
+        => 'If you need any help or support while developing with ZF2, you may reach us via IRC: %s#zftalk on Freenode%s. We\'d love to hear any questions or feedback you may have regarding the beta releases. Alternatively, you may subscribe and post questions to the %smailing lists%s.',
 
     #: ../view/application/index/index.phtml:24
-    'Ping us on IRC' => 'Ping us on IRC',
+    '__label_ping_on_irc'
+        => 'Ping us on IRC',
 
     #: ../view/error/index.phtml:1
-    'An error occurred' => 'An error occurred',
+    '__title_error_application_error'
+        => 'An error occurred',
 
     #: ../view/error/index.phtml:8
-    'Additional information' => 'Additional information',
+    #: ../view/error/404.phtml:53
+    '__title_error_more_info'
+        => 'Additional information',
 
-    #: ../view/error/index.phtml:11
+    #: ../view/error/index.phtml:15
     #: ../view/error/index.phtml:35
-    'File' => 'File',
+    #: ../view/error/404.phtml:56
+    #: ../view/error/404.phtml:80
+    '__label_file'
+        => 'File',
 
     #: ../view/error/index.phtml:15
     #: ../view/error/index.phtml:39
-    'Message' => 'Message',
+    #: ../view/error/404.phtml:60
+    #: ../view/error/404.phtml:84
+    '__label_message'
+        => 'Message',
 
     #: ../view/error/index.phtml:19
     #: ../view/error/index.phtml:43
-    #: ../view/error/404.phtml:55
-    'Stack trace' => 'Stack trace',
+    #: ../view/error/404.phtml:63
+    #: ../view/error/404.phtml:88
+    '__label_stack_trace'
+        => 'Stack trace',
 
     #: ../view/error/index.phtml:29
-    'Previous exceptions' => 'Previous exceptions',
+    #: ../view/error/404.phtml:74
+    '__title_error_previous_exception'
+        => 'Previous exceptions',
 
     #: ../view/error/index.phtml:58
-    'No Exception available' => 'No Exception available',
+    #: ../view/error/404.phtml:103
+    '__title_error_no_exception_available'
+        => 'No Exception available',
 
     #: ../view/error/404.phtml:1
-    'A 404 error occurred' => 'A 404 error occurred',
+    '__title_error_404_error'
+        => 'A 404 error occurred',
 
     #: ../view/error/404.phtml:10
-    'The requested controller was unable to dispatch the request.'
-    => 'The requested controller was unable to dispatch the request.',
+    '__message_error_controller_cannot_dispatch'
+        => 'The requested controller was unable to dispatch the request.',
 
     #: ../view/error/404.phtml:13
-    'The requested controller could not be mapped to an existing controller class.'
-    => 'The requested controller could not be mapped to an existing controller class.',
+    '__message_error_controller_not_found'
+        => 'The requested controller could not be mapped to an existing controller class.',
 
     #: ../view/error/404.phtml:16
-    'The requested controller was not dispatchable.'
-    => 'The requested controller was not dispatchable.',
+    '__message_error_controller_invalid'
+        => 'The requested controller was not dispatchable.',
 
     #: ../view/error/404.phtml:19
-    'The requested URL could not be matched by routing.'
-    => 'The requested URL could not be matched by routing.',
+    '__message_error_router_no_match_found'
+        => 'The requested URL could not be matched by routing.',
 
     #: ../view/error/404.phtml:22
-    'We cannot determine at this time why a 404 was generated.'
-    => 'We cannot determine at this time why a 404 was generated.',
+    '__message_error_404_unknown_error'
+        => 'We cannot determine at this time why a 404 was generated.',
 
     #: ../view/error/404.phtml:34
-    'Controller' => 'Controller',
+    '__label_controller'
+        => 'Controller',
 
     #: ../view/error/404.phtml:41
-    'resolves to %s' => 'resolves to %s',
+    '__message_resolves_to'
+        => 'resolves to %s',
 
     #: ../view/error/404.phtml:51
-    'Exception' => 'Exception',
+    'Exception'
+        => 'Exception',
 ];
