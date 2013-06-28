1698d657abfce0668b16954bdd16657669ca5ed4
Ptah Dunbar <ptah@piratedunbar.com>
2013-06-28 21:19:40 +0200

    Cleanup Application/Module.php

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -1,22 +1,15 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application;
 
-use Zend\ModuleManager\Feature\FormElementProviderInterface;
-use Zend\ModuleManager\Feature\ServiceProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\EventManager\EventInterface;
 
 use Zend\ModuleManager\Feature\BootstrapListenerInterface,
     Zend\ModuleManager\Feature\AutoloaderProviderInterface,
-    Zend\ModuleManager\Feature\ConfigProviderInterface
+    Zend\ModuleManager\Feature\ConfigProviderInterface,
+    Zend\ModuleManager\Feature\ServiceProviderInterface,
+    Zend\ModuleManager\Feature\FormElementProviderInterface,
 ;
 
 /**
