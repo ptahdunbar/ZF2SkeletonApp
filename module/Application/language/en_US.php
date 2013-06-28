b53474f50d3ff3fa4f094cf06f18c34b3df1b385
Ptah Dunbar <ptah@piratedunbar.com>
2013-06-28 11:49:05 +0200

    Initial import of base ZF2 skeleton with some opinionated tweaks.

---
+++ module/Application/language/en_US.php
@@ -0,0 +1,113 @@
+<?php
+
+return [
+    #: ../view/layout/layout.phtml:33
+    'Skeleton Application' => 'Skeleton Application',
+
+    #: ../view/layout/layout.phtml:36
+    'Home' => 'Home',
+
+    #: ../view/layout/layout.phtml:50
+    'All rights reserved.' => 'All rights reserved.',
+
+    #: ../view/application/index/index.phtml:2
+    'Welcome to %sZend Framework 2%s' => 'Welcome to %sZend Framework 2%s',
+
+    #: ../view/application/index/index.phtml:3
+    'Congratulations! You have successfully installed the %sZF2 Skeleton Application%s. You are currently running Zend Framework version %s. This skeleton can serve as a simple starting point for you to begin building your application on ZF2.'
+    => 'Congratulations! You have successfully installed the %sZF2 Skeleton Application%s. You are currently running Zend Framework version %s. This skeleton can serve as a simple starting point for you to begin building your application on ZF2.',
+
+    #: ../view/application/index/index.phtml:3
+    'Congratulations! You have successfully installed the %sZF2 Skeleton Application%s. You are currently running Zend Framework version %s. This skeleton can serve as a simple starting point for you to begin building your application on ZF2.'
+    => 'Congratulations! You have successfully installed the %sZF2 Skeleton Application%s. You are currently running Zend Framework version %s. This skeleton can serve as a simple starting point for you to begin building your application on ZF2.',
+
+    #: ../view/application/index/index.phtml:4
+    'Fork Zend Framework 2 on GitHub' => 'Fork Zend Framework 2 on GitHub',
+
+    #: ../view/application/index/index.phtml:10
+    'Follow Development' => 'Follow Development',
+
+    #: ../view/application/index/index.phtml:11
+    'Zend Framework 2 is under active development. If you are interested in following the development of ZF2, there is a special ZF2 portal on the official Zend Framework website which provides links to the ZF2 %swiki%s, %sdev blog%s, %sissue tracker%s, and much more. This is a great resource for staying up to date with the latest developments!'
+    => 'Zend Framework 2 is under active development. If you are interested in following the development of ZF2, there is a special ZF2 portal on the official Zend Framework website which provides links to the ZF2 %swiki%s, %sdev blog%s, %sissue tracker%s, and much more. This is a great resource for staying up to date with the latest developments!',
+
+    #: ../view/application/index/index.phtml:12
+    'ZF2 Development Portal' => 'ZF2 Development Portal',
+
+    #: ../view/application/index/index.phtml:16
+    'Discover Modules' => 'Discover Modules',
+
+    #: ../view/application/index/index.phtml:17
+    'The community is working on developing a community site to serve as a repository and gallery for ZF2 modules. The project is available %son GitHub%s. The site is currently live and currently contains a list of some of the modules already available for ZF2.'
+    => 'The community is working on developing a community site to serve as a repository and gallery for ZF2 modules. The project is available %son GitHub%s. The site is currently live and currently contains a list of some of the modules already available for ZF2.',
+
+    #: ../view/application/index/index.phtml:18
+    'Explore ZF2 Modules' => 'Explore ZF2 Modules',
+
+    #: ../view/application/index/index.phtml:22
+    'Help &amp; Support' => 'Help &amp; Support',
+
+    #: ../view/application/index/index.phtml:23
+    'If you need any help or support while developing with ZF2, you may reach us via IRC: %s#zftalk on Freenode%s. We\'d love to hear any questions or feedback you may have regarding the beta releases. Alternatively, you may subscribe and post questions to the %smailing lists%s.'
+    => 'If you need any help or support while developing with ZF2, you may reach us via IRC: %s#zftalk on Freenode%s. We\'d love to hear any questions or feedback you may have regarding the beta releases. Alternatively, you may subscribe and post questions to the %smailing lists%s.',
+
+    #: ../view/application/index/index.phtml:24
+    'Ping us on IRC' => 'Ping us on IRC',
+
+    #: ../view/error/index.phtml:1
+    'An error occurred' => 'An error occurred',
+
+    #: ../view/error/index.phtml:8
+    'Additional information' => 'Additional information',
+
+    #: ../view/error/index.phtml:11
+    #: ../view/error/index.phtml:35
+    'File' => 'File',
+
+    #: ../view/error/index.phtml:15
+    #: ../view/error/index.phtml:39
+    'Message' => 'Message',
+
+    #: ../view/error/index.phtml:19
+    #: ../view/error/index.phtml:43
+    #: ../view/error/404.phtml:55
+    'Stack trace' => 'Stack trace',
+
+    #: ../view/error/index.phtml:29
+    'Previous exceptions' => 'Previous exceptions',
+
+    #: ../view/error/index.phtml:58
+    'No Exception available' => 'No Exception available',
+
+    #: ../view/error/404.phtml:1
+    'A 404 error occurred' => 'A 404 error occurred',
+
+    #: ../view/error/404.phtml:10
+    'The requested controller was unable to dispatch the request.'
+    => 'The requested controller was unable to dispatch the request.',
+
+    #: ../view/error/404.phtml:13
+    'The requested controller could not be mapped to an existing controller class.'
+    => 'The requested controller could not be mapped to an existing controller class.',
+
+    #: ../view/error/404.phtml:16
+    'The requested controller was not dispatchable.'
+    => 'The requested controller was not dispatchable.',
+
+    #: ../view/error/404.phtml:19
+    'The requested URL could not be matched by routing.'
+    => 'The requested URL could not be matched by routing.',
+
+    #: ../view/error/404.phtml:22
+    'We cannot determine at this time why a 404 was generated.'
+    => 'We cannot determine at this time why a 404 was generated.',
+
+    #: ../view/error/404.phtml:34
+    'Controller' => 'Controller',
+
+    #: ../view/error/404.phtml:41
+    'resolves to %s' => 'resolves to %s',
+
+    #: ../view/error/404.phtml:51
+    'Exception' => 'Exception',
+];
